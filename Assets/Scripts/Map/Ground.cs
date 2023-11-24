2586d03f5847cb140bc154190cd57ece0f5bd954
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-11-24 15:53:24 +0500

    Update Ground.cs

--- Assets/Scripts/Map/Ground.cs
+++ Assets/Scripts/Map/Ground.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ground : MonoBehaviour
+public class Ground
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
