ed03e0987a6f25090ac2e139aee625bab5dd1db2
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-11-24 15:51:22 +0500

    Update ConsoleInput.cs

--- Assets/Scripts/Input/ConsoleInput.cs
+++ Assets/Scripts/Input/ConsoleInput.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Consolnput : MonoBehaviour
+public class Consolnput
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
