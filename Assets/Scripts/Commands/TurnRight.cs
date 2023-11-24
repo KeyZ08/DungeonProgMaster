0e9e3166ca5d1a62ded1a2ca49884c49d78600d3
wafflelios <107104736+wafflelios@users.noreply.github.com>
2023-11-24 15:49:14 +0500

    Update TurnRight.cs

--- Assets/Scripts/Commands/TurnRight.cs
+++ Assets/Scripts/Commands/TurnRight.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateToRIght : MonoBehaviour
+public class TurnRight
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
