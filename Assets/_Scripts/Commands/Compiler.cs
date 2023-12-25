e145730904ea0bc559937a885d064868b9e1ef06
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2023-12-25 18:01:59 +0500

    добавил IDEContoller

--- Assets/_Scripts/Commands/Compiler.cs
+++ Assets/_Scripts/Commands/Compiler.cs
@@ -1,6 +1,10 @@
 using Microsoft.CSharp;
+using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public static class Compiler
@@ -8,13 +12,22 @@ public static class Compiler
     public static List<string> Compile(string program, string source)
     {
         var script = source;
-        script = script.Replace("// to do", program);
+        script = Regex.Replace(script, @"#region[\s\S]*?#endregion", program);
 
         CSharpCodeProvider provider = new CSharpCodeProvider();
         CompilerResults results = provider.CompileAssemblyFromSource(new CompilerParameters(), script);
+        //Debug.Log(results.Errors.Count);
+        //Debug.Log(results.Errors[0]);
         var cls = results.CompiledAssembly.GetType("Commands.CommandsCompiler");
         var method = cls.GetMethod("Script");
-        return (List<string>)method.Invoke(null, null);
+
+        var timeout = 10;
+        var task = Task.Run(() => (List<string>)method.Invoke(null, null));
+        task.Wait(timeout);
+        if (task.IsCompleted)
+            return task.Result;
+        else
+            throw new Exception("Ваша программа работает очень долго");
     }
 
     public static void TestCompiling(string source)
---
+++ Assets/_Scripts/Commands/IDE/ArraySyntacticConstruction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace IDE
+{
+    public class ArraySyntacticConstruction : ISyntacticConstruction
+    {
+        private readonly string title;
+        private readonly string[] words;
+        private readonly Color32 color;
+
+        public ArraySyntacticConstruction(string title, string[] words, Color32 color)
+        {
+            this.title = title;
+            this.words = words;
+            this.color = color;
+        }
+
+        string ISyntacticConstruction.Title => title;
+        Color32 ISyntacticConstruction.Color => color;
+
+        bool ISyntacticConstruction.CheckWord(string word)
+        {
+            return words.Contains(word);
+        }
+    }
+}
