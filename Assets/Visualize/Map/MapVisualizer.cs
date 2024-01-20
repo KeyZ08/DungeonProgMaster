2eb1ca311c1cc179973896dcd1656f113015a55b
KeyZ <mr.kiriazamorozka@yandex.ru>
2024-01-20 11:00:00 +0500

    bugfix
    исправлен баг  с черным полем и со стеной на 9 уровне

--- Assets/Visualize/Map/MapVisualizer.cs
+++ Assets/Visualize/Map/MapVisualizer.cs
@@ -9,37 +9,43 @@ namespace DPM.UI
     public class MapVisualizer : MonoBehaviour
     {
         [SerializeField] private Tilemap tilemap;
+        [SerializeField] private Tilemap backgroundTilemap;
 
         [Header("Tiles")]
         [SerializeField] private RuleTile ground;
         [SerializeField] private Tile hole;
         [SerializeField] private RuleTile wall;
         [SerializeField] private Tile finish;
+        [SerializeField] private Tile background;
 
         private Dictionary<TileType, TileBase> tileByType;
 
         public void Construct()
         {
             tileByType = new Dictionary<TileType, TileBase>()
-        {
-            { TileType.Ground, ground },
-            { TileType.Hole, hole },
-            { TileType.Wall, wall },
-            { TileType.Finish, finish },
-        };
+            {
+                { TileType.Ground, ground },
+                { TileType.Hole, hole },
+                { TileType.Wall, wall },
+                { TileType.Finish, finish },
+            };
         }
 
         public void DrawMap(Map map)
         {
             tilemap.ClearAllTiles();
+            backgroundTilemap.ClearAllTiles();
             if (tileByType == null) Construct();
 
             for (int row = 0; row < map.MapRows; row++)
             {
                 for (int column = 0; column < map.MapColumns; column++)
                 {
-                    var tile = map.GetTile(new Vector2Int(column, row));
-                    tilemap.SetTile(new Vector3Int(column, row), tileByType[tile]);
+                    var pos = new Vector3Int(column, row, 0);
+                    var tile = map.GetTile((Vector2Int)pos);
+                    tilemap.SetTile(pos, tileByType[tile]);
+                    if (column != 0 && row != 0 && column != map.MapColumns - 1 && row != map.MapRows - 1)
+                        backgroundTilemap.SetTile(pos + Vector3Int.back, background);
                 }
             }
         }
